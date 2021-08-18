c7e6ab503f1466fd67c1bade7b4a1bf97ffdb494
a.khaustov <kosmospacex@gmail.com>
2021-08-18 17:22:30 +0300

    10

--- src/main/java/by/transport/myapp/config/AppConfig.java
+++ src/main/java/by/transport/myapp/config/AppConfig.java
@@ -1,13 +1,50 @@
 package by.transport.myapp.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean(name = "messageResourceSB")
+    public MessageSource messageResource() {
+        ResourceBundleMessageSource messageBundleResrc=new ResourceBundleMessageSource();
+        messageBundleResrc.setBasename("messages");
+        messageBundleResrc.setCacheSeconds(3600);
+        messageBundleResrc.setDefaultEncoding("UTF-8");
+        return messageBundleResrc;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
--- src/main/java/by/transport/myapp/controller/MainController.java
+++ src/main/java/by/transport/myapp/controller/MainController.java
@@ -1,15 +1,24 @@
 package by.transport.myapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Locale;
+
 @Controller
 public class MainController {
 
+    @Autowired
+    @Qualifier("messageResourceSB")
+    MessageSource messageSource;
+
     @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("headerMessage", "Общественный транспорт");
+    public String main(Model model, Locale locale) {
+        model.addAttribute("headerMessage", messageSource.getMessage("headerPT", null, locale));
         return "index";
     }
 }
